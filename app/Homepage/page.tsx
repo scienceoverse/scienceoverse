f3260f82c957e61960db360c6e088d20ad895296
BibinVJ <110018951+BibinVJ@users.noreply.github.com>
2024-03-13 17:35:19 +0530

    fixed image bug

--- app/Homepage/page.tsx
+++ app/Homepage/page.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import '../globals.css'
+
 const page = () => {
   return (
     <div className='container'>
-      <video  width="320" height="240">
-        <source src='../../public/homebg.mp4' type='video/mp4'/>
-      </video>
+      <div className='background-video'>
+        <video width="100%" height="100%" autoPlay loop muted>
+          <source src='/homebg.mp4' type='video/mp4'/>
+        </video>
+      </div>
     </div>
   )
 }
