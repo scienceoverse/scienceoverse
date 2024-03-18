fdfc725515f79e7c6b34497dfdd1fa2ff0ff051b
BibinVJ <110018951+BibinVJ@users.noreply.github.com>
2024-03-18 19:35:55 +0530

    sample commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../../components/Navbar";
 
 const About = () => {
-  return <div>This is the about page here</div>;
+  return <div>This is the about page</div>;
 };
 
 export default About;
